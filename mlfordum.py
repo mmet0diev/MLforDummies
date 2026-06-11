8bd824981181589f1af9bcd0f133401b74eb7c41
mmet0diev <max.metodiev.1999@gmail.com>
2026-06-11 16:47:21 +0300

    added list examples

--- mlfordum.py
+++ mlfordum.py
@@ -91,3 +91,10 @@ def SecretNumber():
 SetA = set(['Red', 'Blue', 'Green', 'Black'])
 SetB = set(['Black', 'Green', 'Yellow', 'Orange'])
 
+# Can run from script and from terminal
+# ListA = [0,1,2,3]
+# ListB = [4,5,6,7]
+# ListA.extend(ListB)
+# ListA
+# ListA.append(-5)
+# ListA
